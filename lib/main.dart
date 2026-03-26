e3335905c7e8c10c09b0b3465c1c6213a1fb7d63
Ali Zaiter <alizaiter@Alis-MacBook-Air.local>
2026-03-26 23:30:55 +0300

    hello world

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ import 'screens/add_friend_screen.dart';
 import 'utils/app_colors.dart';
 import 'utils/page_transitions.dart';
 
+//hello world =
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
